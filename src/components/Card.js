ac5d9f217559235a4fe4e87e605ca997221407c4
Alana <alanayee07@gmail.com>
2021-05-18 15:28:39 -0700

    Card hover and created card-contents

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({title, image, description}) => {
+const Card = ({ title, image, description }) => {
   return (
     <div className="card-container">
       <div className="image-container">
         <img src={image} alt='' />
       </div>
-      <div className="card-title">
-        <h1>{title}</h1>
-      </div>
-      <div className="card-description">
-        <p>{description}</p>
+      <div className="card-contents">
+        <div className="card-title">
+          <h1>{title}</h1>
+        </div>
+        <div className="card-description">
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   )
